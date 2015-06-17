5e3728d93152e19607847727fe00d4b1882e79ed
richy486 <richy486@gmail.com>
2015-06-17 14:08:08 +0200

    Initial Commit

---
+++ ConstraintsHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConstraintsHelper
+//
+//  Created by Richard Adem on 17/06/2015.
+//  Copyright (c) 2015 Richard Adem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ConstraintsHelper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ConstraintsHelper
+//
+//  Created by Richard Adem on 17/06/2015.
+//  Copyright (c) 2015 Richard Adem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
